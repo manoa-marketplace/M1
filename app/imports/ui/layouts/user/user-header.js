63abb157cfd0a9aeb56c06c09625e560b48e2c0a
Vincent Nagoshi <vnagoshi@hawaii.edu>
2017-11-11 14:05:21 -1000

    Updated Header
    Ported in the header. Pressing search will consolelog the search query.
    
    Known Issues:
    Contact seller page removed from header, should be linked on each item's card instead.
    View your listings goes to the generic browse page and will need to be updated later.

--- app/imports/ui/layouts/user/user-header.js
+++ app/imports/ui/layouts/user/user-header.js
@@ -1,8 +1,17 @@
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 Template.User_Header.helpers({
   routeUserName() {
     return FlowRouter.getParam('username');
+  },
+});
+
+Template.User_Header.events({
+  'submit .search-form'(event, instance) {
+    event.preventDefault();
+    const query = event.target.Search.value;
+    console.log(`Search Query: '${query}'`);
   },
 });
